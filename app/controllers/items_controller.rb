93268a8862011d1819e82b8e2dcdafaec4451000
Koi115 <chiiuko115@gmail.com>
2023-05-10 10:48:42 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,13 +21,40 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to "/items/#{item.id}"
+    else
+      @item = item
+      render :edit
+    end
+  end
+
+
+
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :fee_id, :prefecture_id, :period_id,
-                                 :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :fee_id, :prefecture_id, :period_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+
+
 end
