2e5ab4dd715287e85ac42fecc37f3a8a1eca3ff7
Koi115 <chiiuko115@gmail.com>
2023-05-15 16:21:03 +0900

    AWS-S3画像保存

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  #
+  # 2023-05-15 comment  to change the storage from local to S3
+  # config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
