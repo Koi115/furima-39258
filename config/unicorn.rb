3bb935fdd6f417b6aac4d16f0f1c59c2765454ca
Koi115 <chiiuko115@gmail.com>
2023-05-18 09:35:52 +0900

    ポート変更-WebサーバNginx用

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,9 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+# 2023-05-18 Webサーバ Nginx用に変更
+#listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
